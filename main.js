2d3896a77bef4a57ac7c7c6275c53998601ddd49
Gonzalo <gonzalo@estadodigital.com>
2022-10-03 22:05:34 -0300

    Fech api

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-console.log('holasdasd');
+const API_KEY = 'live_tX2syfWhhB9BPDgir3GkaIBo8JusxLWNA9iR1UfcMfFSQcfWl6sNQ4Bmf2CA9Ql9'
 
-const ULR = 'https://api.thecatapi.com/v1/images/search'
+const ULR = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=' + API_KEY
 
 const getCatPic = async () => {
     const res = await fetch(ULR)
     const data = await res.json()
 
-    const img = document.querySelector('img')
-    img.src = data[0].url
+    document.getElementById('1').src = data[0].url
+    document.getElementById('2').src = data[1].url
+    document.getElementById('3').src = data[2].url
 }
 
 document.querySelector('#new-cat').addEventListener('click', getCatPic)
