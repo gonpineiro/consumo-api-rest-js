20e9c52b8a0133e9c83ed130f9f14f044d641cd5
Gonzalo <gonzalo@estadodigital.com>
2022-10-03 22:48:20 -0300

    clase response

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const API_KEY = 'live_tX2syfWhhB9BPDgir3GkaIBo8JusxLWNA9iR1UfcMfFSQcfWl6sNQ4Bmf2CA9Ql9'
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=' + API_KEY
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=' + API_KEY
 
-const ULR = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=' + API_KEY
+const spanError = document.getElementById('error');
 
-const getCatPic = async () => {
-    const res = await fetch(ULR)
-    const data = await res.json()
+const loadRandomMichis = async () => {
+    const res = await fetch(API_URL_RANDOM)
 
-    document.getElementById('img1').src = data[0].url
-    document.getElementById('img2').src = data[1].url
-    document.getElementById('img3').src = data[2].url
+    if (res.status !== 200) {
+        spanError.innerHTML = 'Hubo un error: ' + res.status
+    } else {
+        const data = await res.json()
+        document.getElementById('img1').src = data[0].url
+        document.getElementById('img2').src = data[1].url
+    }
 }
 
-document.querySelector('#new-cat').addEventListener('click', getCatPic)
+const loadFavoritesMichis = async () => {
+    const res = await fetch(API_URL_FAVORITES)
 
-getCatPic();
+    if (res.status !== 200) {
+        spanError.innerHTML = 'Hubo un error: ' + res.status
+    } else {
+        const data = await res.json();
+    }
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
 
 
